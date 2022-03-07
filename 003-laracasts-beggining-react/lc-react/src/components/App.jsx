29f9a7e453b67f990c1f6da706c7ec1fc46b43aa
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-07 14:22:15 -0300

    feat: add and delete todo items using events

--- 003-laracasts-beggining-react/lc-react/src/components/App.jsx
+++ 003-laracasts-beggining-react/lc-react/src/components/App.jsx
@@ -3,57 +3,88 @@ import "../App.css";
 import { useState } from "react";
 
 function App() {
-
   const [todos, setTodos] = useState([
     {
       id: 1,
       title: "Meu primeiro item",
-      isComplete: false
+      isComplete: false,
     },
     {
       id: 2,
       title: "Segundo todo",
-      isComplete: true
+      isComplete: true,
+    },
+  ]);
+  const [idForTodo, setIdForTodo] = useState(3);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+
+    if (todoInput.trim().length === 0) {
+      return;
     }
-  ])
+
+    setTodos([
+      ...todos,
+      {
+        id: idForTodo,
+        title: todoInput,
+        isComplete: false,
+      },
+    ]);
+    setTodoInput("");
+    setIdForTodo((previousId) => previousId + 1);
+  };
+
+  const [todoInput, setTodoInput] = useState("");
+
+  const handleInput = (event) => {
+    setTodoInput(event.target.value);
+  };
+
+  const handleDelete = (todoId) => {
+    setTodos([...todos].filter(item => item.id !== todoId))
+  }
 
   return (
     <div className="todo-app-container">
       <div className="todo-app">
         <h2>Todo App</h2>
-        <form action="#">
+        <form action="#" onSubmit={addTodo}>
           <input
             type="text"
+            value={todoInput}
+            onChange={handleInput}
             className="todo-input"
             placeholder="What do you need to do?"
           />
         </form>
 
         <ul className="todo-list">
-          {todos.map((item, index) => 
-          <li className="todo-item-container" key={item.id}>
-            <div className="todo-item">
-              <input type="checkbox" />
-              <span className="todo-item-label">{item.title}</span>
-              {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
-            </div>
-            <button className="x-button">
-              <svg
-                className="x-button-icon"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </li>
-          )}
+          {todos.map((item, index) => (
+            <li className="todo-item-container" key={item.id}>
+              <div className="todo-item">
+                <input type="checkbox" />
+                <span className="todo-item-label">{item.title}</span>
+                {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
+              </div>
+              <button className="x-button" onClick={() => handleDelete(item.id)}>
+                <svg
+                  className="x-button-icon"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </li>
+          ))}
         </ul>
 
         <div className="check-all-container">
--- 003-laracasts-beggining-react/lc-react/src/components/ClassBasedComponent.jsx
+++ 003-laracasts-beggining-react/lc-react/src/components/ClassBasedComponent.jsx
@@ -1,26 +1,50 @@
 import React, { Component } from "react";
 
+// formato anterior para criar componentes
 export default class ClassBasedComponent extends Component {
   constructor(props) {
     super(props);
     // forma de declarar state em componentes baseados em classe
     this.state = {
-        todos: [
-            {
-              id: 1,
-              title: "Meu primeiro item",
-              isComplete: false
-            },
-            {
-              id: 2,
-              title: "Segundo todo",
-              isComplete: true
-            }
-          ]
-    }
+      todos: [
+        {
+          id: 1,
+          title: "Meu primeiro item",
+          isComplete: false,
+        },
+        {
+          id: 2,
+          title: "Segundo todo",
+          isComplete: true,
+        },
+      ],
+    };
   }
 
+  // exemplo de formato para atualizar state
+  addTodo = (event) => {
+    event.preventDefault();
+
+    this.setState((previousState) => {
+      const newTodos = [
+        ...previousState.todos,
+        {
+          id: 4,
+          title: "titulo",
+          isComplete: false,
+        },
+      ];
+
+      return { todos: newTodos };
+    });
+  };
+
   render() {
-    return <div>Componente baseado em classe, para acessar estado: Existem {this.state.todos.length} itens.</div>;
+    return (
+      <div>
+        Componente baseado em classe, para acessar estado: Existem{" "}
+        {this.state.todos.length} itens.
+      </div>
+    );
   }
 }
