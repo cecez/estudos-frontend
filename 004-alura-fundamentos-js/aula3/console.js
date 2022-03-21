29cd867040acac0b4a9ea020eb3bdfeb03e31cf4
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-21 11:00:16 -0300

    docs: console api

---
+++ 004-alura-fundamentos-js/aula3/console.js
@@ -0,0 +1,14 @@
+// https://nodejs.org/api/console.html
+console.log("teste");
+
+console.time("e agora?");
+
+console.debug("minha string");
+
+console.error("um erro");
+
+console.warn("warning");
+
+console.info("info");
+
+console.table([{"id": 1, "um": "uno", "dois": "zwei"}], ["id", "um", "dois"]);
