6e6f31f26928eb04870c430b6a4e3afef60f3610
Cezar Castro Rosa <cecez86@gmail.com>
2022-03-22 10:52:25 -0300

    docs: operators, template string

--- 004-alura-fundamentos-js/aula3/console.js
+++ 004-alura-fundamentos-js/aula3/console.js
@@ -1,7 +1,7 @@
 // https://nodejs.org/api/console.html
 console.log("teste");
 
-console.time("e agora?");
+console.time("tempo_um");
 
 console.debug("minha string");
 
@@ -12,3 +12,7 @@ console.warn("warning");
 console.info("info");
 
 console.table([{"id": 1, "um": "uno", "dois": "zwei"}], ["id", "um", "dois"]);
+
+console.timeEnd("tempo_um");
+
+console.trace("trace_um");
---
+++ 004-alura-fundamentos-js/aula4/operadores.js
@@ -0,0 +1,6 @@
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators
+
+const idade = 6;
+const nome = "João Pedro";
+
+console.info(`Olá, eu tenho um filho chamado ${nome} que tem ${idade} anos.`);
