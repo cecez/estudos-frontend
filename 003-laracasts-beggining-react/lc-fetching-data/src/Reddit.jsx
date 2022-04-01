93a71e4629a1b6de3cf07bbbcc6e03f491e63445
Cezar Castro Rosa <cecez86@gmail.com>
2022-04-01 18:36:19 -0300

    chore: fetching reddit json

--- 003-laracasts-beggining-react/lc-fetching-data/src/Reddit.jsx
+++ 003-laracasts-beggining-react/lc-fetching-data/src/Reddit.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+
 export default function Reddit() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.reddit.com/r/reactjs.json")
+      .then((response) => response.json())
+      .then((json) => setPosts(json.data.children));
+  }, []);
+
   return (
     <div>
       <h1>Reddit</h1>
+      {posts.map((post) => (
+        <div key={post.data.id}>
+          <h2>{post.data.title}</h2>
+          <p>{post.data.selftext}</p>
+        </div>
+      ))}
     </div>
   );
 }
