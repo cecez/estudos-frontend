9cd72d8f4dea160f6b663f6762ec364becbbff7a
Cezar Castro Rosa <cecez86@gmail.com>
2022-04-06 11:27:12 -0300

    feat: fetching/displaying joke chuck norris api

--- 003-laracasts-beggining-react/lc-fetching-data/src/Joke.jsx
+++ 003-laracasts-beggining-react/lc-fetching-data/src/Joke.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
 export default function Joke() {
+  const [joke, setJoke] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
+      .then((json) => {
+        setJoke(json.value);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setIsLoading(false);
+        setErrorMessage(`There was an error: ${error.message}`);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Joke</h1>
+      {isLoading && <div>Loading ...</div>}
+      {errorMessage && <div>{errorMessage}</div>}
+      {joke && <div>{joke}</div>}
     </div>
   );
 }
--- 003-laracasts-beggining-react/lc-fetching-data/src/Reddit.jsx
+++ 003-laracasts-beggining-react/lc-fetching-data/src/Reddit.jsx
@@ -2,16 +2,27 @@ import { useEffect, useState } from "react";
 
 export default function Reddit() {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     fetch("https://www.reddit.com/r/reactjs.json")
       .then((response) => response.json())
-      .then((json) => setPosts(json.data.children));
+      .then((json) => {
+        setPosts(json.data.children);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        setIsLoading(false);
+        setErrorMessage(`There was an error: ${error.message}`);
+      });
   }, []);
 
   return (
     <div>
       <h1>Reddit</h1>
+      {isLoading && <div>Loading ...</div>}
+      {errorMessage && <div>{errorMessage}</div>}
       {posts.map((post) => (
         <div key={post.data.id}>
           <h2>{post.data.title}</h2>
